90d92ec97e77b3086104383098dc6dd3eab81a8f
MadonnaAdel <madonnadel0@gmail.com>
2025-04-29 18:53:29 +0300

    install bcryptjs

--- controllers/CompanyController.js
+++ controllers/CompanyController.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { companyModel } = require("../models/CompanyModel");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const otps = new Map();
